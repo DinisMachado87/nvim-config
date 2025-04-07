4367e4569dbea59e3b22ebdb159688b888b83a7e
Dinis Machado <dinisma@gmail.com>
2025-04-07 13:40:40 +0200

    Change indentation to tabs

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- Set tab and indentation settings
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true -- Ensure spaces instead of tabs
+vim.opt.expandtab = false -- Ensure spaces instead of tabs
 vim.opt.smartindent = true -- Enable smart indenting
 -- Set line numbering and relative line numbers
 vim.opt.number = true
@@ -27,5 +27,10 @@ vim.api.nvim_create_user_command(
 -- Map F2 to insert 42 header
 vim.api.nvim_set_keymap('n', '<F2>', ':Header<CR>', { noremap = true, silent = true })
 
+-- Terminal key mappings
+vim.keymap.set('t', '<C-w>w', [[<C-\><C-n><C-w>w]], { noremap = true })
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
+-- Automatically enter insert mode when opening a terminal
+vim.cmd('autocmd TermOpen * startinsert')
 
